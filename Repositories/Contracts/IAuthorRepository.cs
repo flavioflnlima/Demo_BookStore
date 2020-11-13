b6d874f837dd0269b2a3719c1b31c3d9debd3284
flavioflnlima <flavioflnlima@gmail.com>
2020-11-12 23:04:40 -0300

    add Repository Pattern

---
+++ Repositories/Contracts/IAuthorRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookStore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repositories.Contracts
+{
+    public interface IAuthorRepository : IDisposable
+    {
+        List<Autor> Get();
+        Autor Get(int id);
+        Autor GetByName(string name);
+        void Create(Autor autor);
+        void Update(Autor autor);
+        void Delete(int id);
+    }
+}
